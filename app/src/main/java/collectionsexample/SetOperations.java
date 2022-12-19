dbb0bca667ecf8c437874ebe593a39dbe413ee17
rdharun <dharunr@testvagrant.com>
2022-12-19 12:05:41 +0530

    set operations

---
+++ app/src/main/java/collectionsexample/SetOperations.java
@@ -0,0 +1,18 @@
+package collectionsexample;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+    public static void main(String args[]) {
+        Set<Integer> numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        numbers.forEach(System.out::println);
+    }
+}
+
